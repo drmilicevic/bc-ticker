06ccf0fd0a191c1e098847d55a69a63a4ee88c06
danicar <danicar@bettercollective.com>
2023-05-10 11:48:38 +0200

    Stop ticker on hover

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => '4e0f79445eec01f66798fccb2dcd7f83');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-polyfill'), 'version' => 'd578e19184626b5fe61c4a4f76aa4e9b');
