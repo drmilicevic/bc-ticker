e3b03ce2cf8fd405517f2540aa51434aad26c164
Dragan Milicevic <dmilicevic@bettercollective.com>
2023-05-11 09:34:47 +0200

    Build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '0c24122926751be5afea');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '06edbc8e620b72caa338');
