36941e1b7f7994a0389d357c65ff126da704c4e4
Radenkovic1981 <radenkovic88@yahoo.com>
2023-05-11 09:27:03 +0200

    cleanup

--- bc-ticker.php
+++ bc-ticker.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Plugin Name:       BC Thicker
- * Description:       BC Thicker block
+ * Plugin Name:       BC Ticker
+ * Description:       BC Ticker block
  * Requires at least: 5.7
  * Requires PHP:      7.0
  * Version:           0.1.0
  * Author:            BC WP team Nis
- * Text Domain:       bc-theme
+ * Text Domain:       bc-ticker
  */
 
 require WP_PLUGIN_DIR . '/bc-ticker/Init.php';
